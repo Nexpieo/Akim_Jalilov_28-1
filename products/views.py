6ff8dc746ab5315c93a8f03533b239c7d308f2c4
Nexpieo <akim.jalilovv@mail.ru>
2023-05-14 12:12:11 +0600

    homework 1

---
+++ products/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import HttpResponse
+from datetime import datetime
+
+
+def hello_view(request):
+    if request.method == 'GET':
+        return HttpResponse('Hello! Its my project')
+
+
+def now_date_view(request):
+    if request.method == 'GET':
+        return HttpResponse(datetime.now())
+
+
+def goodby_view(request):
+    if request.method == 'GET':
+        return HttpResponse('Goodby user!')
