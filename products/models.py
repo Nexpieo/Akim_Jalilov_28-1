2fb3a8f409c916be30d47161e4f8cca00a95202d
Nexpieo <akim.jalilovv@mail.ru>
2023-05-30 22:18:13 +0600

    homework 4

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
+    price = models.FloatField(default=0)
 
     def __str__(self):
         return self.title
